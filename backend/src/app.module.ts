1b16888aa634d7cf81d5fa0d3f5b4c001c692a1d
FlorR566 <florodriguez566@gmail.com>
2026-06-26 20:00:11 -0300

    feat: add environment config with @nestjs/config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotesModule } from './notes/notes.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'better-sqlite3',
       database: 'db.sqlite',
